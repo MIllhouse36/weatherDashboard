c04b09fcf814c9f470cd0ec7df747ed818713548
Kevin <millhousekevin@gmail.com>
2020-07-17 23:26:37 -0400

    added logic to reprevent double searching

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// var searched = cityName;
-var searchedCities = [];
-var historyCard = $("#history-card");
 
 
+
+renderHistory();
 $("#form").on("submit", function (event) {
   event.preventDefault();
   var cityName = $("#textarea").val().trim()
@@ -18,7 +17,7 @@ $("#form").on("submit", function (event) {
       $("#currentIcon").attr("src", "https://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png");
       $("#currentIcon").attr("alt", "weather icon");
       var currentUtcSeconds = response.dt;
-        var currentDate = new Date(0); // The 0 there is the key, which sets the date to the epoch
+        var currentDate = new Date(0);
         currentDate.setUTCSeconds(currentUtcSeconds);
         $("#currentDate").text(currentDate);
       var tempF = (response.main.temp - 273.15) * 1.80 + 32;
@@ -35,68 +34,18 @@ $("#form").on("submit", function (event) {
       }).then(function (respond) {
         $("#currentUv").text(respond.current.uvi)
         console.log(respond);
-       
-        // var forecastDays = returning.list
-        // // console.log(forecastDays)
-        // $.each(forecastDays, function(i, item){
-          //   var forcastTemp = (item.main.temp - 273.15) * 1.80 + 32;
-          //   var forcastHumi =  item.main.humidity;
-          //   console.log(forcastTemp.toFixed(0) + "°");
-          //   console.log(forcastHumi + "%");
-          //   var listItem = $("<p>"+forcastHumi+"%"+"</p>")
-          // });
-        
-        
-
-        $("#forecast-1-img").attr("src", "https://openweathermap.org/img/wn/" + respond.daily[1].weather[0].icon + "@2x.png");
-        $("#forecast-1-img").attr("alt", "weather icon");
-        var utcSeconds = respond.daily[1].dt;
-        var date1 = new Date(0); // The 0 there is the key, which sets the date to the epoch
-        date1.setUTCSeconds(utcSeconds);
-        $("#forecast-1-date").text(date1);
-        var tempF1 = (respond.daily[1].temp.day - 273.15) * 1.80 + 32;
-        $("#forecast-1-temp").text(tempF1.toFixed(0) + "°");
-        $("#forecast-1-humi").text(respond.daily[1].humidity + "%");
-
-        $("#forecast-2-img").attr("src", "https://openweathermap.org/img/wn/" + respond.daily[2].weather[0].icon + "@2x.png");
-        $("#forecast-2-img").attr("alt", "weather icon");
-        var utcSeconds = respond.daily[2].dt;
-        var date2 = new Date(0); // The 0 there is the key, which sets the date to the epoch
-        date2.setUTCSeconds(utcSeconds);
-        $("#forecast-2-date").text(date2);
-        var tempF2 = (respond.daily[2].temp.day - 273.15) * 1.80 + 32;
-        $("#forecast-2-temp").text(tempF2.toFixed(0) + "°");
-        $("#forecast-2-humi").text(respond.daily[2].humidity + "%");
-
-        $("#forecast-3-img").attr("src", "https://openweathermap.org/img/wn/" + respond.daily[3].weather[0].icon + "@2x.png");
-        $("#forecast-3-img").attr("alt", "weather icon");
-        var utcSeconds = respond.daily[3].dt;
-        var date3 = new Date(0); // The 0 there is the key, which sets the date to the epoch
-        date3.setUTCSeconds(utcSeconds);
-        $("#forecast-3-date").text(date3);
-        var tempF3 = (respond.daily[3].temp.day - 273.15) * 1.80 + 32;
-        $("#forecast-3-temp").text(tempF3.toFixed(0) + "°");
-        $("#forecast-3-humi").text(respond.daily[3].humidity + "%");
-
-        $("#forecast-4-img").attr("src", "https://openweathermap.org/img/wn/" + respond.daily[4].weather[0].icon + "@2x.png");
-        $("#forecast-4-img").attr("alt", "weather icon");
-        var utcSeconds = respond.daily[4].dt;
-        var date4 = new Date(0); // The 0 there is the key, which sets the date to the epoch
-        date4.setUTCSeconds(utcSeconds);
-        $("#forecast-4-date").text(date4);
-        var tempF4 = (respond.daily[4].temp.day - 273.15) * 1.80 + 32;
-        $("#forecast-4-temp").text(tempF4.toFixed(0) + "°");
-        $("#forecast-4-humi").text(respond.daily[4].humidity + "%");
-
-        $("#forecast-5-img").attr("src", "https://openweathermap.org/img/wn/" + respond.daily[5].weather[0].icon + "@2x.png");
-        $("#forecast-5-img").attr("alt", "weather icon");
-        var utcSeconds = respond.daily[5].dt;
-        var date5 = new Date(0); // The 0 there is the key, which sets the date to the epoch
-        date5.setUTCSeconds(utcSeconds);
-        $("#forecast-5-date").text(date5);
-        var tempF5 = (respond.daily[5].temp.day - 273.15) * 1.80 + 32;
-        $("#forecast-5-temp").text(tempF5.toFixed(0) + "°");
-        $("#forecast-5-humi").text(respond.daily[5].humidity + "%");
+      
+        for (let i = 1; i < 6; i++) {
+        $("#forecast-"+ i +"-img").attr("src", "https://openweathermap.org/img/wn/" + respond.daily[i].weather[0].icon + "@2x.png");
+        $("#forecast-"+ i +"-img").attr("alt", "weather icon");
+        var utcSeconds = respond.daily[i].dt;
+        var date = new Date(0);
+        date.setUTCSeconds(utcSeconds);
+        $("#forecast-"+ i +"-date").text(date);
+        var tempF = (respond.daily[i].temp.day - 273.15) * 1.80 + 32;
+        $("#forecast-"+ i + "-temp").text(tempF.toFixed(0) + "°");
+        $("#forecast-"+ i + "-humi").text(respond.daily[i].humidity + "%");
+        }
         renderHistory(cityName);
       }) 
     })
@@ -106,17 +55,21 @@ $("#form").on("submit", function (event) {
   
 });
 function renderHistory(city){
+  var historyCard = $("#history-card");
+  var searchedCities = [];
     historyCard.text("");
   if (JSON.parse(localStorage.getItem("Searched"))) {
     searchedCities = JSON.parse(localStorage.getItem("Searched"));
+    $.each(searchedCities, function(i, item){
+     var listItem = $("<p>"+ item + "</p>");
+     historyCard.append(listItem);
+     });
   }
-  searchedCities.push(city);
-  
-        $.each(searchedCities, function(i, item){
-         var listItem = $("<p>"+ item + "</p>");
-         historyCard.append(listItem);
-         });
-  
-  
+  if(!searchedCities.includes(city)){
+    searchedCities.push(city);
+  }
+  if(!city){
+    return;
+  }
     localStorage.setItem("Searched", JSON.stringify(searchedCities));
   }
