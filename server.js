bd86d42d3148006459516e3b2c4f047f58593f37
Kevin <millhousekevin@gmail.com>
2021-05-30 17:50:37 -0400

    adding package to deploy

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+app.use(routes);
+
+app.listen(PORT, ()=>{
+  console.log(`Server listening on: http://localhost: ${PORT}`)
+})
